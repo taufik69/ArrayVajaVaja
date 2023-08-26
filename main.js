03029e2210ac9c261837875e15621f7634408f19
Taufik islam <taufik35-2915@diu.edu.bd>
2023-08-26 16:20:24 +0600

    array updating

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // let arr1 = new Array(10);
-let arr1 = [];
+// let arr1 = [];
 // for (let i = 0; i < arr1.length; i++) {
 //   console.log(arr1.fill("taufik"));
 // }
@@ -11,9 +11,22 @@ let arr1 = [];
 //   arr1[i] = randomNumber > 5 ? "x" : "o";
 // }
 
-for (let i = 0; i < arr1.length; i++) {
-  let randomNumber = Math.floor(Math.random() * 10 + 1);
-  arr1[i].push("x");
-}
+// array fill with random number
 
-console.log(arr1);
+// for (let i = 0; i < 10; i++) {
+//   let randomNumber = Math.floor(Math.random() * 10 + 1);
+
+//   arr1[i] = randomNumber > 5 ? "X" : 0;
+// }
+// console.log("arr1 :", arr1);
+
+// array updating with for loop
+// let arr1 = new Array("taufik", "islam");
+// function arrayUpdate(arr1) {
+//   for (let i = 0; i < arr1.length; i++) {
+//     arr1[i] = `${i + 1} . ${arr1[i]} Boss`;
+//   }
+//   return arr1;
+// }
+
+// console.log("arr1 :", arrayUpdate(arr1));
