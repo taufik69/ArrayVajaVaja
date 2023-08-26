e29c977c904b946f61500e0e411cc4df63c32316
Taufik islam <taufik35-2915@diu.edu.bd>
2023-08-26 16:53:22 +0600

    define funtion strore array and then apply loop to call

--- main.js
+++ main.js
@@ -24,9 +24,29 @@
 // let arr1 = new Array("taufik", "islam");
 // function arrayUpdate(arr1) {
 //   for (let i = 0; i < arr1.length; i++) {
-//     arr1[i] = `${i + 1} . ${arr1[i]} Boss`;
+//     arr1[i] = `${i + 1} . ${arr1[i]}`;
 //   }
 //   return arr1;
 // }
 
 // console.log("arr1 :", arrayUpdate(arr1));
+
+// store function in array
+
+// let sum = (a, b) => a + b;
+// let sub = (a, b) => a - b;
+// let multiply = (a, b) => a * b;
+// let div = (a, b) => a / b;
+// let mod = (a, b) => a % b;
+
+// const functionAll = [sum, sub, multiply, div, mod];
+
+// let a = 10;
+// let b = 20;
+
+// for (let i = 0; i < functionAll.length; i++) {
+//   const element = functionAll[i];
+//   const functionName = element.name;
+//   const result = element(a, b);
+//   console.log(`${functionName} = ${result}`);
+// }
