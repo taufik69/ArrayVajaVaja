876b2dc6fa241dedb4af80dc396251237b3de9ce
Taufik islam <taufik35-2915@diu.edu.bd>
2023-09-13 19:46:17 +0600

    reversed arary

--- stackTest/stack.js
+++ stackTest/stack.js
@@ -3,14 +3,14 @@ const stack = new Stack();
 
 // Take a random text
 let text = "taufik";
-let reverseText = "";
 
 for (let i = 0; i < text.length; i++) {
   stack.push(text.charAt(i));
 }
 
-// while (!stack.isArrayEmpty()) {
-//   reverseText += stack.pop();
-// }
-console.log(stack.pop());
-// console.log(reverseText);
+let reverseText = "";
+while (!stack.isArrayEmpty()) {
+  reverseText += stack.pop();
+}
+
+console.log(reverseText);
